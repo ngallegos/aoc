3dde4613faeef4b06500b13e0ff25aa72e618647
Nick Gallegos <nicholas.gallegos@gmail.com>
2018-12-14 12:07:57 -0700

    Initial commit

---
+++ 2018/AoC2018/Tests/Day01.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC2018
+{
+    [TestClass]
+    public class Day01
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
