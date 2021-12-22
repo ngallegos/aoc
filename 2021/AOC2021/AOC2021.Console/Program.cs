f46c877500b45d21422da0890f3d3e2e6c45543d
nwgallegos <nicholas.gallegos@craftjack.com>
2021-12-22 11:48:00 -0700

    AOC 2021 DAY 02-02

--- 2021/AOC2021/AOC2021.Console/Program.cs
+++ 2021/AOC2021/AOC2021.Console/Program.cs
@@ -8,11 +8,20 @@ namespace AOC2021.Console
     {
         static void Main(string[] args)
         {
-            var day = new Day01();
-            var results = day.Part1();
-            var results2 = day.Part2();
-            System.Console.WriteLine($"Increases: {results.increased}\nDecreases: {results.decreased}");
-            System.Console.WriteLine($"Increases: {results2.increased}\nDecreases: {results2.decreased}");
+            System.Console.WriteLine("DAY 01");
+            var day01 = new Day01();
+            var day0101 = day01.Part1();
+            var day0102 = day01.Part2();
+            System.Console.WriteLine($"\tIncreases: {day0101.increased}\tDecreases: {day0101.decreased}");
+            System.Console.WriteLine($"\tIncreases: {day0102.increased}\tDecreases: {day0102.decreased}");
+
+            System.Console.WriteLine("DAY 02");
+            var day02 = new Day02();
+            var day0201 = day02.Part1();
+            var day0202 = day02.Part2();
+            System.Console.WriteLine($"\tX: {day0201.xPosition}\tY: {day0201.yPosition}\tRESULT: {day0201.xPosition * day0201.yPosition}");
+            System.Console.WriteLine($"\tX: {day0202.horizontalPosition}\tY: {day0202.depth}\tAIM:{day0202.aim}\tRESULT: {day0202.horizontalPosition * day0202.depth}");
+            
         }
     }
 }
---
+++ 2021/AOC2021/AOC2021.Modules/Day02.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace AOC2021.Modules
+{
+    public class Day02
+    {
+        
+        private IEnumerable<string> get_input(){
+            
+            var assembly = this.GetType().GetTypeInfo().Assembly;
+            using (var s = assembly.GetManifestResourceStream("AOC2021.Modules.Inputs.day-02-01.txt"))
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                        yield return line;
+                }
+            }
+        }
+        public (int xPosition, int yPosition) Part1()
+        {
+            var commands = get_input().Select(x => new SubmarineCommand(x)).ToArray();
+
+            var currentX = 0;
+            var currentY = 0;
+
+            foreach (var command in commands)
+            {
+                (currentX, currentY) = command.NextPosition01(currentX, currentY);
+            }
+            
+            return (currentX, currentY);
+        }
+
+        public (int horizontalPosition, int depth, int aim) Part2()
+        {
+            var commands = get_input().Select(x => new SubmarineCommand(x)).ToArray();
+
+            var currentX = 0;
+            var currentY = 0;
+            var currentAim = 0;
+
+            foreach (var command in commands)
+            {
+                (currentX, currentY, currentAim) = command.NextPositionWithAim(currentX, currentY, currentAim);
+            }
+            
+            return (currentX, currentY, currentAim);
+        }
+
+        public class SubmarineCommand
+        {
+            public string Direction { get; }
+            public int Distance { get; }
+            public SubmarineCommand(string input)
+            {
+                var commandParts = input.Split(' ');
+                Direction = commandParts[0];
+                Distance = int.Parse(commandParts[1]);
+            }
+
+            public (int x, int y) NextPosition01(int currentX, int currentY)
+            {
+                switch (Direction)
+                {
+                    case "forward":
+                        return (currentX + Distance, currentY);
+                    case "backward":
+                        return (currentX - Distance, currentY);
+                    case "up":
+                        return (currentX, currentY - Distance);
+                    case "down":
+                        return (currentX, currentY + Distance); // Y is depth so increase on down, decrease on up
+                }
+
+                throw new NotImplementedException();
+            }
+            
+            
+            public (int x, int y, int aim) NextPositionWithAim(int currentH, int currentDepth, int currentAim)
+            {
+                switch (Direction)
+                {
+                    case "forward":
+                        return (currentH + Distance, currentDepth + currentAim * Distance, currentAim);
+                    case "backward":
+                        return (currentH - Distance, currentDepth + currentAim * Distance, currentAim);
+                    case "up":
+                        return (currentH, currentDepth, currentAim - Distance);
+                    case "down":
+                        return (currentH, currentDepth, currentAim + Distance); // Y is depth so increase on down, decrease on up
+                }
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
