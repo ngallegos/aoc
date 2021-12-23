c8ed5432644e9b50a98b0d4db9b7b30951e8066c
nwgallegos <nicholas.gallegos@craftjack.com>
2021-12-23 07:09:44 -0700

    Added day 3 placeholder

---
+++ 2021/AOC2021/AOC2021.Modules/Day03.cs
@@ -0,0 +1,15 @@
+﻿namespace AOC2021.Modules
+{
+    public class Day03 : DayBase
+    {
+        public override dynamic Part1()
+        {
+            return "NOT IMPLEMENTED";
+        }
+
+        public override dynamic Part2()
+        {
+            return "NOT IMPLEMENTED";
+        }
+    }
+}
