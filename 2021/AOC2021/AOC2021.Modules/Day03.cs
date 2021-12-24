a6e2ffffe0b2311f6c17cff0e7c605187f55daec
nwgallegos <nicholas.gallegos@craftjack.com>
2021-12-23 19:28:56 -0700

    AOC 2021 DAY 03-01

--- 2021/AOC2021/AOC2021.Modules/Day03.cs
+++ 2021/AOC2021/AOC2021.Modules/Day03.cs
@@ -1,10 +1,34 @@
-﻿namespace AOC2021.Modules
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace AOC2021.Modules
 {
     public class Day03 : DayBase
     {
         public override dynamic Part1()
         {
-            return "NOT IMPLEMENTED";
+            var input = get_input();
+            var inputs = input.Select(x => x.ToArray().Select(c => double.Parse(c.ToString())));
+            var matrix = Matrix<double>.Build.DenseOfRows(inputs);
+            var gammaBits = new List<int>();
+            var epsilonBits = new List<int>();
+            for (int i = 0; i < matrix.ColumnCount; i++)
+            {
+                var mostCommon = matrix.Column(i)
+                    .AsArray()
+                    .GroupBy(x => x)
+                    .OrderByDescending(g => g.Count())
+                    .Select(x => (int)x.Key).First();
+                gammaBits.Add(mostCommon);
+                epsilonBits.Add((mostCommon + 1) % 2);
+            }
+
+            var gammaRate = Convert.ToInt32(string.Join("", gammaBits.Select(x => x.ToString())), 2);
+            var epsilonRate = Convert.ToInt32(string.Join("", epsilonBits.Select(x => x.ToString())), 2);
+
+            return new { gammaRate, epsilonRate, powerConsumption = gammaRate * epsilonRate };
         }
 
         public override dynamic Part2()
