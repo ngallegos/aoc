06a7e08b4105bc96ce105808e90ab146b9df527d
nwgallegos <nicholas.gallegos@craftjack.com>
2022-11-30 14:40:00 -0700

    cleaning up old stuff

--- 2022/AOC2022.Modules/Day01.cs
+++ 2022/AOC2022.Modules/Day01.cs
@@ -12,37 +12,12 @@ namespace AOC2022.Modules
     {
         public override dynamic Part1()
         {
-            var increased = 0;
-            var decreased = 0;
-            var readings = get_input().Select(int.Parse).ToArray();
-            for(int i = 1; i < readings.Length; i++)
-            {
-                if (readings[i] > readings[i - 1])
-                    increased++;
-                else if (readings[i] < readings[i - 1])
-                    decreased++;
-            }
-
-            return new { increased,  decreased };
+            return "Not implemented";
         }
 
         public override dynamic Part2()
         {
-            
-            var increased = 0;
-            var decreased = 0;
-            var readings = get_input().Select(int.Parse).ToArray();
-            for(int i = 3; i < readings.Length; i++)
-            {
-                var sumCurrent = readings[i] + readings[i - 1] + readings[i - 2];
-                var sumPrev = readings[i - 1] + readings[i - 2] + readings[i - 3];
-                if (sumCurrent > sumPrev)
-                    increased++;
-                else if (sumCurrent < sumPrev)
-                    decreased++;
-            }
-
-            return new {increased,  decreased};
+            return "Not implemented";
         }
     }
 }
