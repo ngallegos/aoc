337dc766f36036008018c4931973d26603bd1385
nwgallegos <nicholas.gallegos@craftjack.com>
2022-12-04 06:18:22 -0700

    Completed day 4

---
+++ 2022/AOC2022.Modules/Day04.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC2022.Modules;
+
+public class Day04 : DayBase
+{
+    public override dynamic Part1()
+    {
+        var assignmentPairs = get_input()
+            .Select(x => new CleaningPair(x))
+            .ToList();
+        var pairsDuplicatingWork = 0;
+        foreach (var pair in assignmentPairs)
+        {
+            if (pair.HasDuplicateAssignment)
+                pairsDuplicatingWork++;
+        }
+
+        return new { pairsDuplicatingWork };
+    }
+
+    public override dynamic Part2()
+    {
+        
+        var assignmentPairs = get_input()
+            .Select(x => new CleaningPair(x))
+            .ToList();
+        var pairsWithOverlappingWork = 0;
+        foreach (var pair in assignmentPairs)
+        {
+            if (pair.HasOverlappingAssignment)
+                pairsWithOverlappingWork++;
+        }
+
+        return new { pairsWithOverlappingWork };
+    }
+
+    private class CleaningAssignment
+    {
+        public CleaningAssignment(string input)
+        {
+            var sections = input.Split("-", StringSplitOptions.TrimEntries);
+            StartingSection = int.Parse(sections[0]);
+            EndingSection = int.Parse(sections[1]);
+        }
+        
+        public int StartingSection { get; }
+        public int EndingSection { get; }
+
+        public bool ContainsAssignment(CleaningAssignment other) =>
+            StartingSection <= other.StartingSection && EndingSection >= other.EndingSection;
+
+        public bool OverlapsAssignment(CleaningAssignment other) => !HasNoOverlap(other);
+
+        public bool HasNoOverlap(CleaningAssignment other) =>
+            EndingSection < other.StartingSection || StartingSection > other.EndingSection;
+    }
+
+    private class CleaningPair
+    {
+        public CleaningPair(string input)
+        {
+            var assignments = input.Split(",", StringSplitOptions.TrimEntries);
+            Elf1 = new CleaningAssignment(assignments[0]);
+            Elf2 = new CleaningAssignment(assignments[1]);
+        }
+        public CleaningAssignment Elf1 { get; }
+        public CleaningAssignment Elf2 { get; }
+
+        public bool HasDuplicateAssignment => Elf1.ContainsAssignment(Elf2) || Elf2.ContainsAssignment(Elf1);
+        public bool HasOverlappingAssignment => Elf1.OverlapsAssignment(Elf2) || Elf2.OverlapsAssignment(Elf1);
+    }
+}
