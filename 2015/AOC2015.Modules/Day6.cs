b0521b86cd5c314c38652e7dc7510f02c1d2c144
ngallegos <nicholas.gallegos@gmail.com>
2022-12-15 09:54:28 -0700

    2015 Day 6

--- 2015/AOC2015.Modules/Day6.cs
+++ 2015/AOC2015.Modules/Day6.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
+using AOC2015.Modules.Shared;
 
 namespace AOC2015.Modules;
 
@@ -8,31 +11,116 @@ public class Day6 : DayBase
     public override bool Completed { get; }
 
     private static Regex _instructionRegex =
-        new Regex(@"^(?<type>turn|toggle) ?(?<state>on|off)? (?<start>\d*,\d*) through (?<end>\d*,\d*)$"); 
+        new Regex(@"^(?<type>turn|toggle) ?(?<state>on|off)? (?<start>\d*,\d*) through (?<end>\d*,\d*)$");
+
+    private List<LightingInstruction> _instructions = null;
     public override dynamic Part1()
     {
-        var instructions = new List<LightingInstruction>();
-        var instr = new LightingInstruction("turn on 0,0 through 999,999");
-        var instr2 = new LightingInstruction("toggle 0,0 through 999,0");
-        var instr3 = new LightingInstruction("turn off 499,499 through 500,500");
+        _instructions ??= get_input().Select(x => new LightingInstruction(x))
+            .ToList();
+        var grid = new LightGrid();
+        foreach (var instruction in _instructions)
+        {
+            instruction.ExecutePart1(grid);
+        }
 
-        return "not implemented";
+        return grid;
     }
 
     public override dynamic Part2()
     {
-        throw new System.NotImplementedException();
+        _instructions ??= get_input().Select(x => new LightingInstruction(x))
+            .ToList();
+        var grid = new LightGrid();
+        foreach (var instruction in _instructions)
+        {
+            instruction.ExecutePart2(grid);
+        }
+
+        return grid;
+    }
+
+    private class LightGrid : Grid<int>
+    {
+        public LightGrid()
+        {
+            Initialize(new GridLocation(0, 0), new GridLocation(999, 999));
+        }
+
+        public int LightsLit => _grid.Sum(x => x.Sum());
     }
 
     private class LightingInstruction
     {
+        private enum Action
+        {
+            On,
+            Off,
+            Toggle
+        }
+        
+        private Action _action { get; }
+        private List<(int x, int y)> _targets { get; }
+        
         public LightingInstruction(string input)
         {
             var result = _instructionRegex.Match(input);
             var type = result.Groups["type"].Value;
             var state = result.Groups["state"].Value;
-            var start = result.Groups["start"].Value;
-            var end = result.Groups["end"].Value;
+            var startPoint = result.Groups["start"].Value.Split(',').Select(int.Parse).ToArray();
+            var endPoint = result.Groups["end"].Value.Split(',').Select(int.Parse).ToArray();
+
+            if (type == "toggle")
+                _action = Action.Toggle;
+            else if (state == "on")
+                _action = Action.On;
+            else
+                _action = Action.Off;
+
+            _targets = new List<(int x, int y)>();
+            for (int y = startPoint[1]; y <= endPoint[1]; y++)
+            {
+                for (int x = startPoint[0]; x <= endPoint[0]; x++)
+                    _targets.Add((x, y));
+            }
+        }
+
+        public void ExecutePart1(LightGrid grid)
+        {
+            foreach (var target in _targets)
+            {
+                switch (_action)
+                {
+                    case Action.On:
+                        grid[target.y][target.x] = 1;
+                        break;
+                    case Action.Off:
+                        grid[target.y][target.x] = 0;
+                        break;
+                    case Action.Toggle:
+                        grid[target.y][target.x] = (grid[target.y][target.x] + 1) % 2;
+                        break;
+                }
+            }
+        }
+        
+        public void ExecutePart2(LightGrid grid)
+        {
+            foreach (var target in _targets)
+            {
+                switch (_action)
+                {
+                    case Action.On:
+                        grid[target.y][target.x] += 1;
+                        break;
+                    case Action.Off:
+                        grid[target.y][target.x] = Math.Max(grid[target.y][target.x] - 1, 0);
+                        break;
+                    case Action.Toggle:
+                        grid[target.y][target.x] += 2;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ 2015/AOC2015.Modules/Shared/Grid.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2015.Modules.Shared;
+
+public class Grid<T>
+{
+    protected List<List<T>> _grid;
+    public List<T> this[int index] => _grid[index];
+    protected int Length => _grid.Count;
+    protected int _xMinOffset;
+    protected int _yMinOffset;
+
+    protected void Initialize(GridLocation upperLeft, GridLocation lowerRight, List<GridLocation<T>> initialValues = null, T defaultValue = default)
+    {
+        initialValues ??= new List<GridLocation<T>>();
+        _xMinOffset = upperLeft.X;
+        _yMinOffset = upperLeft.Y;
+        var maxY = lowerRight.Y - upperLeft.Y;
+        var maxX = lowerRight.X - upperLeft.X;
+        _grid = Enumerable.Range(0, maxY + 1)
+            .Select(y => Enumerable.Range(0, maxX + 1)
+                .Select(x =>
+                {
+                    var location = initialValues.FirstOrDefault(v => v.X == x + _xMinOffset && v.Y == y + _yMinOffset);
+                    if (location != null)
+                        return location.Value;
+                    return defaultValue;
+                }).ToList()).ToList();
+    }
+
+    public List<string> Render(Func<T, string> transform = null)
+    {
+        transform = transform ?? (x => x.ToString()); 
+        return _grid.Select(y => string.Join("", y.Select(x => transform(x)))).ToList();
+    }
+}
+
+public class GridLocation
+{
+    public GridLocation(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; protected set; }
+    public int Y { get; protected set; }
+
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+}
+
+public class GridLocation<T> : GridLocation
+{
+    public GridLocation(int x, int y, T value) : base(x, y)
+    {
+        Value = value;
+    }
+    public T Value { get; protected set; }
+}
