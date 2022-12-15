ba672fbb577d6ce867aaef2d6845c9ec56dfa40a
ngallegos <nicholas.gallegos@gmail.com>
2022-12-15 16:12:06 -0700

    Day 15 part 2 wip

--- 2022/AOC2022.Modules/Day15.cs
+++ 2022/AOC2022.Modules/Day15.cs
@@ -13,28 +13,86 @@ public class Day15 : DayBase
     {
         var sampleSensors = get_sample()
             .Select(Sensor.Parse).ToList();
-        var grid = new SensorGrid(sampleSensors);
+        var actualSensors = get_input().Select(Sensor.Parse).ToList();
+        var sampleGrid = new SensorGrid(sampleSensors);
+        var actualGrid = new SensorGrid(actualSensors);
 
-        return grid.Render();
+        return new
+        {
+            SampleEliminatedPositions = sampleGrid.GetEliminatedPositions(10).eliminatedPositions.Count,
+            //ActualEliminatedPositiongs = actualGrid.GetEliminatedPositions(2000000).eliminatedPositions.Count
+        };
     }
 
     public override dynamic Part2()
     {
-        throw new System.NotImplementedException();
+        var sampleSensors = get_sample()
+            .Select(Sensor.Parse).ToList();
+        var actualSensors = get_input().Select(Sensor.Parse).ToList();
+        var sampleGrid = new SensorGrid(sampleSensors);
+        var actualGrid = new SensorGrid(actualSensors);
+
+        return new
+        {
+            SampleFrequency = sampleGrid.IsolateDistressBeacon(20, 20),
+            //ActualEliminatedPositiongs = actualGrid.GetEliminatedPositions(2000000).eliminatedPositions.Count
+        };
     }
 
-    private class SensorGrid : Grid<char>
+    private class SensorGrid
     {
+        private List<Sensor> _sensors;
         public SensorGrid(List<Sensor> sensors)
         {
-            var sensorsAndBeacons = sensors.Cast<GridLocation<char>>()
-                .Concat(sensors.Select(x => x.ClosestBeacon)).ToList();
-            var minX = sensorsAndBeacons.Min(x => x.X);
-            var maxX = sensorsAndBeacons.Max(x => x.X);
-            var minY = sensorsAndBeacons.Min(x => x.Y);
-            var maxY = sensorsAndBeacons.Max(x => x.Y);
-            
-            Initialize(new GridLocation(minX, minY), new GridLocation(maxX, maxY), sensorsAndBeacons, '.');
+            _sensors = sensors;
+        }
+
+        public (List<(int x, int y)> eliminatedPositions, int lastPotentialX) GetEliminatedPositions(int row, int? minX = null, int? maxX = null)
+        {
+            minX ??= _sensors.Min(x => x.X - x.ManhattanDistance);
+            maxX ??= _sensors.Max(x => x.X + x.ManhattanDistance);
+            var eliminatedPositions = new List<(int x, int y)>();
+            var beaconLocations = _sensors.Select(x => x.ClosestBeacon.ToString()).ToList();
+            var lastPotentialX = minX.Value;
+            for (int x = minX.Value; x <= maxX.Value; x++)
+            {
+                var eliminationFound = false;
+                foreach (var sensor in _sensors)
+                {
+                    var positionString = $"({x},{row})";
+                    if (sensor.LocationWithinManhattanDistance(x, row) && !beaconLocations.Contains(positionString))
+                    {
+                        eliminationFound = true;
+                        eliminatedPositions.Add((x, row));
+                        break;
+                    }
+                }
+                if (!eliminationFound)
+                    lastPotentialX = x;
+            }
+            return (eliminatedPositions.DistinctBy(item => $"({item.x},{item.y})").ToList(), lastPotentialX);
+        }
+
+        public int IsolateDistressBeacon(int maxX, int maxY)
+        {
+            var minX = 0;
+            var minY = 0;
+            for (int y = minY; y <= maxY; y++)
+            {
+                var results = GetEliminatedPositions(y, minX, maxX);
+                Console.WriteLine(results.eliminatedPositions.Count);
+                if (results.eliminatedPositions.Count <= maxX)
+                {
+                    return CalculateTuningFrequency(results.lastPotentialX, y);
+                }
+            }
+
+            return -1;
+        }
+
+        private int CalculateTuningFrequency(int x, int y)
+        {
+            return (x * 4000000) + y;
         }
     }
 
@@ -56,7 +114,18 @@ public class Day15 : DayBase
         public Sensor(int x, int y, char value, Beacon closestBeacon) : base(x, y, value)
         {
             ClosestBeacon = closestBeacon;
-            ManhattanDistance = Math.Abs(x - closestBeacon.X) + Math.Abs(y - closestBeacon.Y);
+            ManhattanDistance = CalculateManhattanDistance(closestBeacon.X, closestBeacon.Y);
+        }
+
+        private int CalculateManhattanDistance(int x2, int y2)
+        {
+            return Math.Abs(X - x2) + Math.Abs(Y - y2) + 1;
+        }
+
+        public bool LocationWithinManhattanDistance(int x, int y)
+        {
+            var d = CalculateManhattanDistance(x, y);
+            return d <= ManhattanDistance;
         }
         
         public Beacon ClosestBeacon { get; private set; }
