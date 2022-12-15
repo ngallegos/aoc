3d542bbf9e256b2cf3fcb08dacc927e41e1710b2
ngallegos <nicholas.gallegos@gmail.com>
2022-12-14 17:02:28 -0700

    2015 Day 4

---
+++ 2015/AOC2015.Modules/Day04.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AOC2015.Modules;
+
+public class Day04 : DayBase
+{
+    public override bool Completed => true;
+    public override dynamic Part1()
+    {
+        var (hash, lowestNumber) = FindAdventCoinHash("yzbqklnj", "00000");
+
+        return new
+        {
+            hash,
+            lowestNumber
+        };
+    }
+
+    public override dynamic Part2()
+    {
+        var (hash, lowestNumber) = FindAdventCoinHash("yzbqklnj", "000000");
+
+        return new
+        {
+            hash,
+            lowestNumber
+        };
+    }
+
+    private (string hash, long lowestNumber) FindAdventCoinHash(string secretKey, string hashPrefix)
+    {
+        var currentNumber = 0;
+        var hash = ComputeMD5Hash($"{secretKey}{currentNumber}");
+        while (!hash.StartsWith(hashPrefix))
+        {
+            currentNumber++;
+            hash = ComputeMD5Hash($"{secretKey}{currentNumber}");
+        }
+
+        return (hash, currentNumber);
+    }
+    private MD5 _md5 = MD5.Create();
+
+    private string ComputeMD5Hash(string secretKey)
+    {
+        var hashBytes = _md5.ComputeHash(Encoding.UTF8.GetBytes(secretKey));
+        return string.Join("", hashBytes.Select(x => x.ToString("X2")));
+    }
+}
