f9ea2fb7a128265eba9550205e379c87188b69f7
ngallegos <nicholas.gallegos@gmail.com>
2022-12-20 17:28:09 -0700

    2015 day 7 wip

--- 2015/AOC2015.Modules/Day07.cs
+++ 2015/AOC2015.Modules/Day07.cs
@@ -1,4 +1,8 @@
-﻿namespace AOC2015.Modules;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC2015.Modules;
 
 public class Day07 : DayBase
 {
@@ -11,5 +15,52 @@ public class Day07 : DayBase
     public override dynamic Part2()
     {
         throw new System.NotImplementedException();
+    }
+
+    private class Circuit
+    {
+        private List<Wire> _wires = new List<Wire>();
+        
+        
+
+        public void ParseInstruction(string instruction)
+        {
+            var parts = instruction.Split(" -> ");
+            var wire = _wires.FirstOrDefault(x => x.ID == parts[1]);
+            if (wire == null)
+            {
+                wire = new Wire(new Signal(parts[0])) { ID = parts[1] };
+                _wires.Add(wire);
+            }
+        }
+    }
+    
+    private class Wire
+    {
+        public string ID { get; set; }
+        private Signal _signal { get; set; }
+
+        public Wire(Signal signal)
+        {
+            _signal = signal;
+        }
+    }
+
+    private class Signal
+    {
+        public bool Active => _getSignal?.Invoke() != null;
+
+        private Wire _leftWire = null;
+        private Wire _rightWire = null;
+
+        private Func<int> _getSignal;
+
+        public Signal(string input)
+        {
+            if (int.TryParse(input, out int rawsignal))
+            {
+                _getSignal = () => rawsignal;
+            }
+        }
     }
 }
