1dfd988276d9677269187a4368406e6ccda820aa
ngallegos <nicholas.gallegos@gmail.com>
2022-12-21 13:55:36 -0700

    2015 day 7

--- 2015/AOC2015.Modules/Day07.cs
+++ 2015/AOC2015.Modules/Day07.cs
@@ -9,58 +9,107 @@ public class Day07 : DayBase
     public override bool Ignore { get; }
     public override dynamic Part1()
     {
-        throw new System.NotImplementedException();
+        var sampleCircuit = new Circuit(get_sample());
+        var actualCircuit = new Circuit(get_input());
+
+        return new { 
+            sample = sampleCircuit.GetAllSignals()
+                .OrderBy(x => x.wire)
+                .Select(x => $"{x.wire}: {x.signal}"),
+            
+            actualAWireSignal = actualCircuit.GetSignal("a")
+        };
     }
 
     public override dynamic Part2()
     {
-        throw new System.NotImplementedException();
+        var actualCircuit = new Circuit(get_input());
+        actualCircuit.Override("b", 3176);
+
+        return new {
+            actualAWireSignal = actualCircuit.GetSignal("a")
+        };
     }
 
     private class Circuit
     {
-        private List<Wire> _wires = new List<Wire>();
-        
-        
+        private List<Wire> _wires;
 
-        public void ParseInstruction(string instruction)
+        public Circuit(IEnumerable<string> instructions)
         {
-            var parts = instruction.Split(" -> ");
-            var wire = _wires.FirstOrDefault(x => x.ID == parts[1]);
-            if (wire == null)
-            {
-                wire = new Wire(new Signal(parts[0])) { ID = parts[1] };
-                _wires.Add(wire);
-            }
+            _wires = instructions.Select(x => new Wire(x)).ToList();
+        }
+
+        public void Override(string wire, ushort signal)
+        {
+            _wires.First(x => x.ID == wire).SetSignal(signal);
+        }
+
+        public ushort GetSignal(string signal)
+        {
+            if (ushort.TryParse(signal, out var raw))
+                return raw;
+            return _wires.First(x => x.ID == signal).GetSignal(this);
+        }
+
+        public List<(string wire, ushort signal)> GetAllSignals()
+        {
+            return _wires.Select(w => (w.ID, w.GetSignal(this))).ToList();
         }
     }
     
     private class Wire
     {
-        public string ID { get; set; }
-        private Signal _signal { get; set; }
+        public string ID { get; private set; }
+        private string _signalExpression;
+        private ushort? _computedSignal;
 
-        public Wire(Signal signal)
+        public Wire(string instruction)
         {
-            _signal = signal;
+            var parts = instruction.Split(" -> ");
+            ID = parts[1];
+            _signalExpression = parts[0];
         }
-    }
 
-    private class Signal
-    {
-        public bool Active => _getSignal?.Invoke() != null;
-
-        private Wire _leftWire = null;
-        private Wire _rightWire = null;
-
-        private Func<int> _getSignal;
-
-        public Signal(string input)
+        public void SetSignal(ushort signal)
         {
-            if (int.TryParse(input, out int rawsignal))
+            _computedSignal = signal;
+        }
+
+        public ushort GetSignal(Circuit circuit)
+        {
+            if (_computedSignal.HasValue)
+                return _computedSignal.Value;
+
+            if (_signalExpression.Contains("AND"))
             {
-                _getSignal = () => rawsignal;
+                var signals = _signalExpression.Split(" AND ");
+                _computedSignal = (ushort)(circuit.GetSignal(signals[0]) & circuit.GetSignal(signals[1]));
             }
+            else if (_signalExpression.Contains("OR"))
+            {
+                var signals = _signalExpression.Split(" OR ");
+                _computedSignal = (ushort)(circuit.GetSignal(signals[0]) | circuit.GetSignal(signals[1]));
+            }
+            else if (_signalExpression.Contains("LSHIFT"))
+            {
+                var signals = _signalExpression.Split(" LSHIFT ");
+                _computedSignal = (ushort)(circuit.GetSignal(signals[0]) << int.Parse(signals[1]));
+            }
+            else if (_signalExpression.Contains("RSHIFT"))
+            {
+                var signals = _signalExpression.Split(" RSHIFT ");
+                _computedSignal = (ushort)(circuit.GetSignal(signals[0]) >> int.Parse(signals[1]));
+            }
+            else if (_signalExpression.Contains("NOT"))
+            {
+                var signal = _signalExpression.Replace("NOT ", "");
+                _computedSignal = (ushort)~circuit.GetSignal(signal);
+            }
+            else // its a raw signal
+                _computedSignal = circuit.GetSignal(_signalExpression);
+
+            return _computedSignal.Value;
         }
     }
 }
