d3f064b795a23e911d95679db33004a566d1077f
ngallegos <nicholas.gallegos@gmail.com>
2022-12-22 13:17:22 -0700

    2015 day 9 setup

---
+++ 2015/AOC2015.Modules/Day09.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace AOC2015.Modules;
+
+/// <summary>
+/// https://www.reddit.com/r/adventofcode/comments/3w192e/day_9_solutions/
+/// https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
+/// </summary>
+public class Day09 : DayBase
+{
+    public override bool Ignore { get; }
+    public override dynamic Part1()
+    {
+        var pairs = get_sample()
+            .Select(x => new TownPair(x))
+            .ToList();
+        throw new System.NotImplementedException();
+    }
+
+    public override dynamic Part2()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private class TownPair
+    {
+        public string StartTown { get; private set; }
+        public string DestTown { get; private set; }
+        public int Distance { get; private set; }
+
+        public TownPair(string input)
+        {
+            var parts = input.Split(" = ");
+            var towns = parts[0].Split(" to ");
+            StartTown = towns[0];
+            DestTown = towns[1];
+            Distance = int.Parse(parts[1]);
+        }
+    }
+}
