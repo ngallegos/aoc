b2e7705692a11c291ce2417f086d763d6876a0f4
ngallegos <nicholas.gallegos@gmail.com>
2022-12-25 12:10:37 -0700

    2022-23 stopping point

--- 2022/AOC2022.Modules/Day23.cs
+++ 2022/AOC2022.Modules/Day23.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AOC2022.Modules.Shared;
 
@@ -28,6 +29,45 @@ public class Day23 : DayBase
         (-1, 0),
         (-1, 1),
     };
+
+    private class Scan
+    {
+        private bool[,] _grid;
+
+        public Scan(List<string> lines)
+        {
+            _grid = new bool[lines[0].Length, lines.Count];
+            for (int y = 0; y < lines.Count; y++)
+            {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    _grid[x, y] = lines[y][x] == '#';
+                }
+            }
+        }
+        
+        private bool[] Column(int column)
+        {
+            return Enumerable.Range(0, _grid.GetLength(0))
+                .Select(x => _grid[x, column])
+                .ToArray();
+        }
+        
+        private bool[] Row(int row)
+        {
+            return Enumerable.Range(0, _grid.GetLength(1))
+                .Select(x => _grid[row, x])
+                .ToArray();
+        }
+
+        public void Resize(int newWidth, int newHeight)
+        {
+            _grid = _grid.ResizeArray(newWidth, newHeight, 1, 1);
+        }
+        
+    }
+
+    
     
     private class Elf
     {
@@ -39,10 +79,26 @@ public class Day23 : DayBase
             Location = initialLocation;
         }
 
-        public GridLocation ConsiderNewLocation(List<Elf> elves)
-        {
-            var adjacent = _neighbors.Select(l => this.Location + l);
-            
+        // public GridLocation ConsiderNewLocation(List<Elf> elves)
+        // {
+        //     var adjacent = _neighbors.Select(l => this.Location + l);
+        //     //foreach (var elf in elves)
+        //         
+        // }
+    }
+}
+
+public static class Extensions
+{
+    public static T[,] ResizeArray<T>(this T[,] original, int newWidth, int newHeight, int offsetX = 0, int offsetY = 0)
+    {
+        T[,] newArray = new T[newWidth, newHeight];
+        int width = original.GetLength(0);
+        int height = original.GetLength(1);
+        for (int x = 0; x < width; x++) {
+            Array.Copy(original, x * height, newArray, (x + offsetX) * newHeight + offsetY, height);
         }
+
+        return newArray;
     }
 }
