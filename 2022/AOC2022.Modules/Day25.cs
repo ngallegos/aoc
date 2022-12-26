3629c8818dc1f6c40498ad4307f5addaf4d52182
ngallegos <nicholas.gallegos@gmail.com>
2022-12-26 06:24:29 -0700

    2022-25 part 1 complete

--- 2022/AOC2022.Modules/Day25.cs
+++ 2022/AOC2022.Modules/Day25.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AOC2022.Modules;
 
 public class Day25 : DayBase
 {
-    public override bool Ignore { get; }
+    public override bool Ignore => true;
     public override dynamic Part1()
     {
         var sampleValues = get_sample()
             .Select(x => new { snafu = x, base10 = SNAFU.SNAFUToDecimal(x) })
             .ToList();
         var sampleSum = sampleValues.Sum(x => x.base10);
+        
+        var actualValues = get_input()
+            .Select(x => new { snafu = x, base10 = SNAFU.SNAFUToDecimal(x) })
+            .ToList();
+        var actualSum = actualValues.Sum(x => x.base10);
 
-        foreach (var sv in sampleValues)
-        {
-            Console.WriteLine($"{sv.snafu}\t{sv.base10}\t {SNAFU.DecimalToSNAFU(sv.base10)}");
-        }
+        // foreach (var sv in sampleValues)
+        // {
+        //     Console.WriteLine($"{sv.snafu}\t{sv.base10}\t {SNAFU.DecimalToSNAFU(sv.base10)}");
+        // }
         
         return new
         {
             sampleSum,
-            sampleSumSnafu = SNAFU.DecimalToSNAFU(sampleSum)
+            sampleSumSnafu = SNAFU.DecimalToSNAFU(sampleSum),
+            actualSum,
+            actualSumSnafu = SNAFU.DecimalToSNAFU(actualSum)
         };
     }
 
@@ -33,7 +41,7 @@ public class Day25 : DayBase
 
     private class SNAFU
     {
-        private static List<(char snafuChar, int decimalValue)> _map = new List<(char snafuChar, int decimalValue)>
+        private static List<(char snafuChar, long decimalValue)> _map = new List<(char snafuChar, long decimalValue)>
         {
             ('2', 2),
             ('1', 1),
@@ -42,36 +50,44 @@ public class Day25 : DayBase
             ('=', -2),
         };
 
-        private static int _snafuBase = 5;
+        private static long _snafuBase = 5L;
 
-        public static string DecimalToSNAFU(int value)
+        /// <summary>
+        /// I was trying to do this backwards - starting with the highest digits
+        /// Got help from <see href="https://www.reddit.com/r/adventofcode/comments/zur1an/comment/j1o63ly/?utm_source=share&utm_medium=web2x&context=3">this paste</see>
+        /// on the <see href="https://www.reddit.com/r/adventofcode/comments/zur1an/2022_day_25_solutions/">solutions megathread</see>
+        /// </summary>
+        public static string DecimalToSNAFU(long value)
         {
-            var quotient = 0;
-            var pow = 0;
-            do
-            {
-                quotient = (int)value / ((int)Math.Pow(_snafuBase, pow));
+            if (value == 0) return "0";
+            var sb = new StringBuilder();
 
-                pow++;
-            } while (quotient != 0);
-           
-            pow--;
-            for (int i = pow; i <= 0; i--)
+            while (value != 0)
             {
-                // TODO
+                var remainder = value % _snafuBase;
+                value /= 5;
+
+                if (remainder > 2)
+                {
+                    value++;
+                    remainder -= 5;
+                }
+
+                var c = _map.First(x => x.decimalValue == remainder);
+                sb.Insert(0, c.snafuChar);
             }
-            Console.WriteLine($"Highest power of {_snafuBase}: {pow}");
-            return null;
+
+            return sb.ToString();
         }
 
-        public static int SNAFUToDecimal(string snafu)
+        public static long SNAFUToDecimal(string snafu)
         {
             var snafuChars = snafu.ToCharArray().Reverse().ToArray();
-            var decimalValue = 0;
+            var decimalValue = 0L;
             for (int i = 0; i < snafuChars.Length; i++)
             {
                 var snafuVal = _map.First(x => x.snafuChar == snafuChars[i]).decimalValue;
-                var exp = (int)Math.Pow(_snafuBase, i);
+                var exp = (long)Math.Pow(_snafuBase, i);
                 decimalValue += snafuVal * exp;
             }
 
