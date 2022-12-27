388ffc38e72cf9600d5b0c544e594afd50905552
ngallegos <nicholas.gallegos@gmail.com>
2022-12-27 06:22:55 -0700

    2022-16 with help. still not getting the right answer on part 2

--- 2022/AOC2022.Modules/Day16.cs
+++ 2022/AOC2022.Modules/Day16.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using AOC2022.Modules.Shared;
 
 namespace AOC2022.Modules;
 
@@ -11,29 +12,128 @@ public class Day16 : DayBase
     public override bool Ignore => false;
     public override dynamic Part1()
     {
-        var sampleValves = get_sample().Select(x => new Valve(x)).ToList();
-        foreach (var valve in sampleValves)
+        return "ignored";
+        var valves = get_input().Select(x => new Valve(x)).ToList();
+        foreach (var valve in valves)
         {
-            valve.LinkValveTunnels(sampleValves);
+            valve.LinkValveTunnels(valves);
+        }
+
+        // help from : https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0yvgu8/?utm_source=share&utm_medium=web2x&context=3
+        var distances = new List<(string startValve, string endValve, int distance)>();
+        
+        // Compute shortest distance between points
+        foreach (var startValve in valves)
+        {
+            foreach (var endValve in valves)
+            {
+                var bfsResult =
+                BFS<Valve>.Search(startValve, 
+                    v => v.TunnelDestinations, 
+                    v => v.Name == endValve.Name);
+                distances.Add((startValve.Name, endValve.Name, bfsResult.Count - 1));
+            }
         }
         
-        // TODO bfs on all valves with nonzero flow rate and start with max flow rates first
+        int max = 0;
+        var curPath = new List<Valve>();
+        var paths = new Dictionary<HashSet<Valve>, int>();
+        int available = valves.Count(v => v.FlowRate > 0 && !v.IsOpen);
         
-        return sampleValves;
+        backtrack(valves, distances, 30, valves.First(x => x.Name == "AA"), 
+            0, 0, available, ref max, curPath, paths);
+        
+        return new
+        {
+            max
+        };
     }
-
+    
     public override dynamic Part2()
     {
-        throw new System.NotImplementedException();
+        var valves = get_input().Select(x => new Valve(x)).ToList();
+        foreach (var valve in valves)
+        {
+            valve.LinkValveTunnels(valves);
+        }
+
+        // help from : https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0yvgu8/?utm_source=share&utm_medium=web2x&context=3
+        var distances = new List<(string startValve, string endValve, int distance)>();
+        
+        // Compute shortest distance between points
+        foreach (var startValve in valves)
+        {
+            foreach (var endValve in valves)
+            {
+                var bfsResult =
+                    BFS<Valve>.Search(startValve, 
+                        v => v.TunnelDestinations, 
+                        v => v.Name == endValve.Name);
+                distances.Add((startValve.Name, endValve.Name, bfsResult.Count - 1));
+            }
+        }
+        
+        int max = 0;
+        var curPath = new List<Valve>();
+        var paths = new Dictionary<HashSet<Valve>, int>();
+        int available = valves.Count(v => v.FlowRate > 0 && !v.IsOpen);
+        
+        backtrack(valves, distances, 26, valves.First(x => x.Name == "AA"), 
+            0, 0, available, ref max, curPath, paths);
+        // Part 2
+        foreach(var a in paths.Keys.OrderByDescending(o => paths[o]))
+        {
+            foreach (var b in paths.Keys.OrderByDescending(o => paths[o]))
+            {
+                if (paths[a] + paths[b] < max)
+                    goto exit;
+
+                if(!a.Overlaps(b))
+                {
+                    max = paths[a] + paths[b];
+                }
+            }
+        }
+        exit:
+
+        Console.WriteLine(max);
+        return new
+        {
+            max
+        };
     }
 
-    private enum ValveState
+    
+    // help from : https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0yvgu8/?utm_source=share&utm_medium=web2x&context=3
+    void backtrack(List<Valve> valves, List<(string startValve, string endValve, int distance)> dist, int time, Valve cur, 
+        int pressure, int cumulpressure,
+        int available, ref int max, List<Valve> curPath, Dictionary<HashSet<Valve>, int> paths)
     {
-        Open,
-        Closed
-    }
+        if (time == 0 || available == 0)
+        {
+            max = Math.Max(max, cumulpressure + (pressure * time));
+            return;
+        }
 
-    record TunnelMap(int[,] distances, Valve[] valves);
+        paths.Add(new HashSet<Valve>(curPath), cumulpressure + (pressure * time));
+
+        foreach (var n in valves)
+        {
+            if (n == cur  || n.IsOpen || n.FlowRate == 0)
+                continue;
+
+            var ttg = dist.First(x => x.startValve == n.Name && x.endValve == cur.Name).distance + 1;
+            if(ttg > time)
+            {
+                ttg = time;
+            }
+            n.IsOpen = true;
+            curPath.Add(n);
+            backtrack(valves, dist, time - ttg, n, pressure + n.FlowRate, cumulpressure + (pressure * ttg), available - 1, ref max, curPath, paths);
+            curPath.RemoveAt(curPath.Count- 1);
+            n.IsOpen = false;
+        }
+    }
     
     private class Valve : IEquatable<Valve>
     {
@@ -42,10 +142,10 @@ public class Day16 : DayBase
         public string Name { get; }
         public int FlowRate { get; }
         public int MinutesOpened { get; private set; }
-        public ValveState State { get; private set; } = ValveState.Closed;
+        public bool IsOpen { get; set; }
         public int TotalPressureRelease => MinutesOpened * FlowRate;
         public List<string> _tunnelDestinationNames = new List<string>();
-        private List<Valve> TunnelDestinations { get; set; } = new List<Valve>();
+        public List<Valve> TunnelDestinations { get; set; } = new List<Valve>();
 
         public Valve(string input)
         {
@@ -60,18 +160,6 @@ public class Day16 : DayBase
             var relevantValves = allValves.Where(v => _tunnelDestinationNames.Contains(v.Name));
             TunnelDestinations.AddRange(relevantValves);
         }
-
-        public void OpenValve()
-        {
-            State = ValveState.Open;
-        }
-
-        public void MinuteElapsed()
-        {
-            if (State == ValveState.Open)
-                MinutesOpened++;
-        }
-
         public bool Equals(Valve other)
         {
             if (ReferenceEquals(null, other)) return false;
@@ -90,6 +178,16 @@ public class Day16 : DayBase
         public override int GetHashCode()
         {
             return (Name != null ? Name.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Valve left, Valve right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Valve left, Valve right)
+        {
+            return !Equals(left, right);
         }
     }
 }
