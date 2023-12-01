ee63fc6ce614b48bbfdc55657933eddf69115e31
Nick Gallegos <nicholas.gallegos@craftjack.com>
2023-12-01 13:23:42 -0700

    2023 - Day 01 part one done. WIP part 2

---
+++ 2023/AOC2023.Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using AOC2023.Modules;
+using Newtonsoft.Json;
+
+namespace AOC2023.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var baseDayType = typeof(DayBase);
+            var days = typeof(DayBase).Assembly.GetTypes()
+                .Where(x => x.IsSubclassOf(baseDayType) && !x.IsAbstract)
+                .OrderBy(x => x.Name);
+
+            foreach (var dayType in days)
+            {
+                var dayNumber = int.Parse(dayType.Name.Replace("Day", ""));
+                var day = Activator.CreateInstance(dayType) as DayBase;
+                if (day.Ignore)
+                    continue;
+                System.Console.WriteLine($"-----DAY {dayNumber:00}---------------------------------------------\n");
+                var s = new Stopwatch();
+                s.Start();
+                try
+                {
+                    var part1Results = await day.Part1Async();
+                    System.Console.WriteLine(
+                        $"PART 01:\n{JsonConvert.SerializeObject(part1Results, Formatting.Indented)}");
+                }
+                catch (NotImplementedException)
+                {
+                    System.Console.WriteLine("PART 01:\nNot Implemented");
+                }
+                s.Stop();
+                var p1Elapsed = s.ElapsedMilliseconds;
+                System.Console.WriteLine($"Part 01:\t{p1Elapsed}ms");
+                
+                s.Restart();
+
+                try
+                {
+                    var part2Results = await day.Part2Async();
+                    System.Console.WriteLine(
+                        $"PART 02:\n{JsonConvert.SerializeObject(part2Results, Formatting.Indented)}");
+                }
+                catch (NotImplementedException)
+                {
+                    System.Console.WriteLine("PART 02:\nNot Implemented");
+                }
+                s.Stop();
+                var p2Elapsed = s.ElapsedMilliseconds;
+                System.Console.WriteLine($"Part 02:\t{p2Elapsed}ms");
+                System.Console.WriteLine($"Total:\t\t{p1Elapsed + p2Elapsed}ms");
+
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2023/AOC2023.Modules/DayBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AOC2023.Modules
+{
+    public abstract class DayBase
+    {
+        public abstract bool Ignore { get; }
+
+        protected IEnumerable<string> get_sample()
+        {
+            return get_input(type: "Sample");
+        }
+
+        protected IEnumerable<string> get_input(int partNumber = 1, string type = "Input")
+        {
+            var t = new Stopwatch();
+            t.Start();
+            var assembly = this.GetType().GetTypeInfo().Assembly;
+            var dayType = this.GetType();
+            var dayNumber = int.Parse(dayType.Name.Replace("Day", ""));
+            var resources = assembly.GetManifestResourceNames();
+            var resouceName = resources.FirstOrDefault(x => x.Contains(type) && (x.EndsWith($"day-{dayNumber:00}-{partNumber:00}.txt")
+                                                            || x.EndsWith($"day-{dayNumber:00}-01.txt")));
+            using (var s = assembly.GetManifestResourceStream($"{resouceName}"))
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                        yield return line;
+                }
+            }
+            t.Stop();
+            Console.WriteLine($"Input parsing:\t{t.ElapsedMilliseconds}ms");
+        }
+        
+        protected IEnumerable<T> get_input<T>(Func<string, T> transform, int partNumber = 1, string type = "Input")
+        {
+            var inputs = get_input(1, type);
+            var t = new Stopwatch();
+            t.Start();
+            foreach (var input in inputs)
+            {
+                yield return transform(input);
+            }
+            t.Stop();
+            Console.WriteLine($"Input transform:\t{t.ElapsedMilliseconds}ms");
+        }
+
+        public abstract dynamic Part1();
+        public abstract dynamic Part2();
+
+        public virtual async Task<dynamic> Part1Async()
+        {
+            return await new ValueTask<dynamic>(Part1());
+        }
+        public virtual async Task<dynamic> Part2Async()
+        {
+            return await new ValueTask<dynamic>(Part2());
+        }
+    }
+}
