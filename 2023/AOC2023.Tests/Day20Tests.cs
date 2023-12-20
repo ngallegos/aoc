29ad7a18e5b1299631f5e573d97eb9b8277f5027
Nick Gallegos <nicholas.gallegos@craftjack.com>
2023-12-20 10:13:11 -0500

    day 20 still need to implement module types

--- 2023/AOC2023.Tests/Day20Tests.cs
+++ 2023/AOC2023.Tests/Day20Tests.cs
@@ -1,10 +1,13 @@
+using System.Text.RegularExpressions;
+
 namespace AOC2023.Tests;
 
 public class Day20Tests : TestBase
 {
     protected override void SolvePart1_Sample()
     {
-        throw new NotImplementedException();
+        var modules = get_sample(Module.Create)
+            .ToList();
     }
 
     protected override void SolvePart1_Actual()
@@ -22,12 +25,75 @@ public class Day20Tests : TestBase
         throw new NotImplementedException();
     }
 
+    private class FlipFlop : Module
+    {
+        protected override Pulse Process(Pulse inputPulse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    private class Conjunction : Module
+    {
+        protected override Pulse Process(Pulse inputPulse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    private class Broadcast : Module
+    {
+
+        protected override Pulse Process(Pulse inputPulse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
     private abstract class Module
     {
-        protected List<Module> Inputs { get; private set; } = new();
-        protected List<Module> Outputs { get; private set; } = new();
+        public string ID { get; set; }
+        protected List<Module> Inputs { get; set; } = new();
+        protected List<Module> Outputs { get; set; } = new();
+        public string[] OutputIDS = Array.Empty<string>();
         public event EventHandler<InputProcessedEventArgs> InputProcessed;
 
+        private static Regex _definitionRegex = new Regex(@"^(?<type>[%&]?)(?<id>[a-z]+) -> (?<outputs>.*)$");
+
+        public static Module Create(string definition)
+        {
+            var match = _definitionRegex.Match(definition);
+            if (!match.Success)
+                throw new ArgumentException($"Invalid definition: {definition}");
+            var id = match.Groups["id"].Value;
+            var outputs = match.Groups["outputs"].Value.Split(", ", StringSplitOptions.TrimEntries);
+            Type type;
+            switch (match.Groups["type"].Value)
+            {
+                case "%":
+                    type = typeof(FlipFlop);
+                    break;
+                case "&":
+                    type = typeof(Conjunction);
+                    break;
+                default:
+                    type = typeof(Broadcast);
+                    break;
+            }
+
+            var module = Activator.CreateInstance(type) as Module;
+            if (module == null)
+                throw new Exception("Could not create module");
+            module.ID = id;
+            module.OutputIDS = outputs;
+            return module;
+        }
+
+        public void AssignInputs(List<Module> inputs)
+        {
+            
+        }
+        
         protected abstract Pulse Process(Pulse inputPulse);
         public void ProcessInput(Pulse inputPulse)
         {
