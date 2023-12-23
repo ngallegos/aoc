772113b4aab7eaae9c22086984a7a7906acd43b3
Nick Gallegos <nicholas.gallegos@craftjack.com>
2023-12-23 15:07:54 -0500

    Setting up day 17 and 18

---
+++ 2023/AOC2023.Tests/Day17Tests.cs
@@ -0,0 +1,24 @@
+namespace AOC2023.Tests;
+
+public class Day17Tests : TestBase
+{
+    protected override void SolvePart1_Sample()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void SolvePart1_Actual()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void SolvePart2_Sample()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void SolvePart2_Actual()
+    {
+        throw new NotImplementedException();
+    }
+}
