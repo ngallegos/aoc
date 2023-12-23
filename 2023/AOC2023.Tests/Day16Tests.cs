9a223af1d97e39845a0f3caf0f8121c85c097939
Nick Gallegos <nicholas.gallegos@craftjack.com>
2023-12-23 13:42:56 -0500

    Day 16 part 2 done :)

--- 2023/AOC2023.Tests/Day16Tests.cs
+++ 2023/AOC2023.Tests/Day16Tests.cs
@@ -9,30 +9,30 @@ public class Day16Tests : TestBase
     protected override void SolvePart1_Sample()
     {
         var grid = get_sample_grid((c, p) => new Tile(c, p));
-        var energizedTiles = GetEnergizedTiles(grid);
+        var energizedTiles = GetEnergizedTiles(grid, (0, 0, Direction.Right));
         energizedTiles.ShouldBe(46);
     }
     
     protected override void SolvePart1_Actual()
     {
         var grid = get_input_grid((c, p) => new Tile(c, p));
-        var energizedTiles = GetEnergizedTiles(grid);
+        var energizedTiles = GetEnergizedTiles(grid, (0, 0, Direction.Right));
         energizedTiles.ShouldBe(7996);
     }
-    private int GetEnergizedTiles(Tile[,] grid)
+    private int GetEnergizedTiles(Tile[,] grid, (int x, int y, Direction laserDirection) start)
     {
         var positions = new Queue<(int x, int y, Direction laserDirection)>();
         var upperBounds = (x: grid.GetLength(0) - 1, y: grid.GetLength(1) - 1);
-        positions.Enqueue((0, 0, Direction.Right));
+        positions.Enqueue(start);
         while (positions.Any())
         {
             var p = positions.Dequeue();
             var tile = grid[p.x, p.y];
-            Trace.WriteLine($"({p.x},{p.y}) {p.laserDirection}");
+            //Trace.WriteLine($"({p.x},{p.y}) {p.laserDirection}");
             var nextPositions = tile.NextLaserPositions(p.laserDirection, (0, 0), upperBounds);
             foreach (var np in nextPositions)
                 positions.Enqueue(np);
-            Print(grid);
+            //Print(grid);
             
         }
 
@@ -42,12 +42,45 @@ public class Day16Tests : TestBase
     }
     protected override void SolvePart2_Sample()
     {
-        throw new NotImplementedException();
+        var grid = get_sample_grid((c, p) => new Tile(c, p));
+        var energizedTiles = GetMaxEnergizedTiles(grid);
+        energizedTiles.ShouldBe(51);
     }
 
     protected override void SolvePart2_Actual()
     {
-        throw new NotImplementedException();
+        var grid = get_input_grid((c, p) => new Tile(c, p));
+        var energizedTiles = GetMaxEnergizedTiles(grid);
+        energizedTiles.ShouldBe(8239);
+    }
+
+    private int GetMaxEnergizedTiles(Tile[,] grid)
+    {
+        var upperBounds = (x: grid.GetLength(0) - 1, y: grid.GetLength(1) - 1);
+        var maxEnergizedTiles = 0;
+        var startPositions = new List<(int x, int y, Direction laserDirection)>();
+        startPositions.AddRange(Enumerable.Range(0, upperBounds.x + 1)
+            .Select(x => (x, 0, Direction.Down)));
+        startPositions.AddRange(Enumerable.Range(0, upperBounds.x + 1)
+            .Select(x => (x, upperBounds.y, Direction.Up)));
+        startPositions.AddRange(Enumerable.Range(0, upperBounds.y + 1)
+            .Select(y => (0, y, Direction.Right)));
+        startPositions.AddRange(Enumerable.Range(0, upperBounds.y + 1)
+            .Select(y => (upperBounds.x, y, Direction.Left)));
+
+        var tasks = startPositions.Select(x => Task.Run(() =>
+        {
+            var freshGrid = new Tile[upperBounds.x + 1,upperBounds.y + 1];
+            for (int i = 0; i <= upperBounds.x; i++)
+                for (int j = 0; j <= upperBounds.y; j++)
+                    freshGrid[i, j] = new Tile(grid[i, j].Type, (i, j));
+            var energizedTiles = GetEnergizedTiles(freshGrid, x);
+            Trace.WriteLine($"({x.x}, {x.y}) {x.laserDirection} => {energizedTiles}");
+            return energizedTiles;
+        }));
+        var maxTiles = Task.WhenAll(tasks).GetAwaiter().GetResult();
+        
+        return maxTiles.Max();
     }
 
     
