909464ebbea6fe0f4db02e01bfc9f3c164364196
Nick Gallegos <nicholas.gallegos@craftjack.com>
2023-12-25 21:37:45 -0500

    Revert "Day 25 committing this because I want the record, but it doesn't work and I give up"
    This reverts commit 6caea9dcf31c629df3cd2770fc909cc424980d47.

--- 2023/AOC2023.Tests/Day25Tests.cs
+++ 2023/AOC2023.Tests/Day25Tests.cs
@@ -1,103 +1,59 @@
 using System.Text.RegularExpressions;
-using Shouldly;
 
 namespace AOC2023.Tests;
 
 public class Day25Tests : TestBase
 {
-    
-    //https://aoc.csokavar.hu/?day=25
     protected override void SolvePart1_Sample()
     {
-        var input = get_sample().ToList();
-        // call Karger's algorithm in a loop until it finds a cut with 3 edges:
-        var (cutSize, c1, c2) = FindCut(input);
-        while (cutSize != 3) {
-            (cutSize, c1, c2) = FindCut(input);
+        var s = get_sample(x =>
+        {
+            var parts = x.Split(":", StringSplitOptions.TrimEntries);
+            return new
+            {
+                vertex = parts[0],
+                edges = parts[1].Split(' ', StringSplitOptions.TrimEntries)
+                    .Select(d => new List<string> { parts[0], d }.OrderBy(e => e).ToList())
+                    .ToList()
+            };
+        }).ToList();
+        var vertices = s.Select(x => x.vertex).ToList();
+        var edges = s.SelectMany(x => x.edges).DistinctBy(x => string.Join("", x)).ToList();
+        var vCount = vertices.Count;
+        while (vCount > 4)
+        {
+            (vertices, edges) = contract((vertices, edges));
+            vCount = vertices.Count;
         }
-        var groupProduct = c1 * c2; 
-        groupProduct.ShouldBe(54);
+        
+        // not sure what to do with the result yet.
+
     }
 
     private static Random _rnd = new Random(DateTimeOffset.UtcNow.Millisecond);
+    private (List<string> v, List<List<string>> e) contract((List<string> v, List<List<string>> e) graph)
+    {
+        var r = _rnd.Next(graph.e.Count);
+        var edgeToCut = graph.e[r];
+        var newVertex = edgeToCut[0] + edgeToCut[1];
+        var newVertexes = graph.v.Where(x => !edgeToCut.Contains(x)).ToList();
+        var newEdges = graph.e.Where(x => newVertex != (x[0] + x[1])).ToList();
+        newEdges.ForEach(ne =>
+        {
+            ne.ForEach(nv =>
+            {
+                if (nv == edgeToCut[0] || nv == edgeToCut[1])
+                    nv = newVertex;
+            });
+        });
+        return (newVertexes, newEdges);
+    }
 
     protected override void SolvePart1_Actual()
     {
         throw new NotImplementedException();
     }
-    // https://en.wikipedia.org/wiki/Karger%27s_algorithm
-    // The Karger algorithm returns the size of one 'cut' of the graph. 
-    // It's a randomized algorithm that is 'likely' to find the minimal cut 
-    // in a reasonable time. The algorithm is extended to also return the sizes 
-    // of the two components separated by the cut.
-    (int size, int c1, int c2) FindCut(List<string> input) {
- 
-        // super inefficient version.
- 
-        var graph = Parse(input);
-        var componentSize = graph.Keys.ToDictionary(k => k, _ => 1);
- 
-        while (graph.Count > 2) {
-            // choose a random edge u-v to contract.
-            var u = graph.Keys.ElementAt(_rnd.Next(graph.Count));
-            var v = graph[u][_rnd.Next(graph[u].Count)];
- 
-            // Merge nodes u and v by removing 'v' from the graph and rebinding 
-            // the edges of 'v' so that they start from 'u' now. 
-            // There are no multiple edges between two nodes in the original
-            // graph, but this algorithm will introduce some.
- 
-            // rebind
-            foreach (var neighbour in graph[v].ToArray())
-            {
-                if (graph.TryGetValue(neighbour, out var value))
-                {
-                    value.Remove(v);
-                    value.Add(u);
-                }
-            }
- 
-            // 'u' inherits 'v'-s edges
-            graph[u] = graph[u].Concat(graph[v]).Distinct().ToList();
 
-            // update component size 
-            componentSize[u] = componentSize[u] + componentSize[v];
-          
-            // 'v' can be removed now
-            graph.Remove(v);
-            componentSize.Remove(v);
-        }
- 
-        // at the end we have just two nodes with some edges between them,
-        // the number of those edges equals to the size of the cut
-        var nodeA = graph.Keys.First();
-        var nodeB = graph.Keys.Last();
-        return (graph[nodeA].Count, componentSize[nodeA], componentSize[nodeB]);
-    }
- 
-    // Returns an adjacency list representation of the input. Edges are recorded 
-    // both ways, unlike in the input which contains them in one direction only.
-    Dictionary<string, List<string>> Parse(List<string> input) {
-        Dictionary<string, List<string>> res = new();
- 
-        var registerEdge = (string u, string v) => {
-            if (!res.ContainsKey(u)) {
-                res[u] = new();
-            }
-            res[u].Add(v);
-        };
- 
-        foreach (var line in input) {
-            var parts = line.Split(": ");
-            var u = parts[0];
-            var nodes = parts[1].Split(' ');
-            foreach (var v in nodes) {
-                registerEdge(u, v);
-                registerEdge(v, u);
-            }
-        }
-        return res;
-    }
     protected override void SolvePart2_Sample()
     {
         throw new NotImplementedException();
