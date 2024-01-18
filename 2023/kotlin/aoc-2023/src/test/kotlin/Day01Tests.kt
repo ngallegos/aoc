e8733e51c479d02bafa7076cd56c6af05669b3fb
Nick Gallegos <nicholas.gallegos@craftjack.com>
2024-01-17 19:34:00 -0700

    Added inputs to the kotlin test resources folder and got the first test working

--- 2023/kotlin/aoc-2023/src/test/kotlin/Day01Tests.kt
+++ 2023/kotlin/aoc-2023/src/test/kotlin/Day01Tests.kt
@@ -1,6 +1,25 @@
+import java.io.File
+import kotlin.test.assertEquals
+
 class Day01Tests : TestBase() {
     override fun SolvePart1_Sample() {
         assert(true);
+        val expectedCalibrationSum = 142;
+        val calibrationValues = javaClass.getResource("sample-01-01.txt")
+            ?.readText()
+            ?.split("\n")
+            ?.map { GetNumberFromFirstAndLastDigitsInString(it) };
+        val calibrationSum = calibrationValues?.sum();
+
+        assertEquals(expectedCalibrationSum, calibrationSum);
+    }
+
+    private fun GetNumberFromFirstAndLastDigitsInString(input: String) : Int {
+        val regex = Regex("""\D""");
+        val justNumbers = input.replace(regex, "");
+        val charArray = charArrayOf(justNumbers.first(), justNumbers.last());
+        var firstAndLast = String(charArray);
+        return firstAndLast.toInt();
     }
 
     override fun SolvePart1_Actual() {
