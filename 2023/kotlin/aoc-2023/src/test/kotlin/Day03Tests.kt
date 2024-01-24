cb2d0a43557ef2da6b9be0a7ea8436f4a5d3cb9d
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-01-24 15:28:36 -0700

    Almost got part day 3 part 2 done. Pretty sure I need to account for part numbers that are adjacent to more than one gear...

--- 2023/kotlin/aoc-2023/src/test/kotlin/Day03Tests.kt
+++ 2023/kotlin/aoc-2023/src/test/kotlin/Day03Tests.kt
@@ -34,17 +34,59 @@ class Day03Tests : TestBase() {
     }
 
     override fun solvePart2Sample() {
-        TODO("Not yet implemented")
+        // Arrange
+        val expectedGearRatioSum = 467835
+        val engineParts = mutableListOf<EnginePart>()
+        val lines = getSample().toList()
+        lines.forEachIndexed { index, line -> engineParts.addAll(EnginePart.getPartsFromSchematicLine(line, index, lines)) }
+
+        // Act
+        val validEngineParts = engineParts.filter { it.isAdjacentToGear }
+            .groupBy { it.gearLocation }
+            .filter { it.value.count() == 2 && it.key.isNotEmpty() }
+            .map{
+                object {
+                    val gearID = it.key
+                    val gearRatio = it.value[0].partNumber * it.value[1].partNumber
+                }
+            }
+        val actualSum = validEngineParts
+            .sumOf { it.gearRatio }
+
+        // Assert
+        assertEquals(expectedGearRatioSum, actualSum)
     }
 
     override fun solvePart2Actual() {
-        TODO("Not yet implemented")
+        // Arrange
+        val expectedGearRatioSum = 467835
+        val engineParts = mutableListOf<EnginePart>()
+        val lines = getInput().toList()
+        lines.forEachIndexed { index, line -> engineParts.addAll(EnginePart.getPartsFromSchematicLine(line, index, lines)) }
+
+        // Act
+        val validEngineParts = engineParts.filter { it.isAdjacentToGear }
+            .groupBy { it.gearLocation }
+            .filter { it.value.count() == 2 && it.key.isNotEmpty() }
+            .map{
+                object {
+                    val gearID = it.key
+                    val gearRatio = it.value[0].partNumber * it.value[1].partNumber
+                }
+            }
+        val actualSum = validEngineParts
+            .sumOf { it.gearRatio }
+
+        // Assert
+        assertEquals(expectedGearRatioSum, actualSum)
     }
 
     class EnginePart{
         var partNumber: Int = 0
         var lineNumber: Int = 0
         var isAdjacentToSymbol: Boolean = false
+        var isAdjacentToGear: Boolean = false;
+        var gearLocation: String = "";
 
         companion object {
             fun getPartsFromSchematicLine(line: String, lineNumber: Int, lines: List<String>): List<EnginePart>{
@@ -58,16 +100,28 @@ class Day03Tests : TestBase() {
                         ep.partNumber = it.value.toInt()
                         ep.lineNumber = lineNumber
                         if (it.range.first > 0){
-                            if (line[it.range.first - 1] != '.') {
+                            val precedingChar = line[it.range.first - 1];
+                            if (precedingChar != '.') {
                                 ep.isAdjacentToSymbol = true
+                                if (precedingChar == '*') // gear
+                                {
+                                    ep.isAdjacentToGear = true;
+                                    ep.gearLocation = "${lineNumber}-${it.range.last - 1}";
+                                }
                                 return@map ep
                             }
                             possibleSymbolIndices.add(it.range.first - 1)
                         }
                         possibleSymbolIndices.addAll(it.range)
                         if (it.range.last < line.length - 1) {
+                            val succeedingChar = line[it.range.last + 1];
                             if (line[it.range.last + 1] != '.'){
                                 ep.isAdjacentToSymbol = true
+                                if (succeedingChar == '*') // gear
+                                {
+                                    ep.isAdjacentToGear = true;
+                                    ep.gearLocation = "${lineNumber}-${it.range.last + 1}";
+                                }
                                 return@map  ep
                             }
                             possibleSymbolIndices.add(it.range.last + 1)
@@ -75,16 +129,28 @@ class Day03Tests : TestBase() {
 
                         if (lineNumber > 0){
                             val prevLineToCheck = lines[lineNumber - 1].substring(possibleSymbolIndices.first(), possibleSymbolIndices.last() + 1)
-                            if (prevLineToCheck.any { c -> validSymbolRegex.matches(c.toString()) }){
+                            val symbolChar = prevLineToCheck.find { c-> validSymbolRegex.matches(c.toString()) }
+                            if (symbolChar != null){
                                 ep.isAdjacentToSymbol = true
+                                if (symbolChar == '*') // gear
+                                {
+                                    ep.isAdjacentToGear = true;
+                                    ep.gearLocation = "${lineNumber - 1}-${lines[lineNumber - 1].indexOf(symbolChar)}";
+                                }
                                 return@map ep
                             }
                         }
 
                         if (lineNumber < lines.count() - 1){
                             val nextLineToCheck = lines[lineNumber + 1].substring(possibleSymbolIndices.first(), possibleSymbolIndices.last() + 1)
-                            if (nextLineToCheck.any { c -> validSymbolRegex.matches(c.toString()) }){
+                            val symbolChar = nextLineToCheck.find { c-> validSymbolRegex.matches(c.toString()) }
+                            if (symbolChar != null){
                                 ep.isAdjacentToSymbol = true
+                                if (symbolChar == '*') // gear
+                                {
+                                    ep.isAdjacentToGear = true;
+                                    ep.gearLocation = "${lineNumber + 1}-${lines[lineNumber + 1].indexOf(symbolChar)}";
+                                }
                                 return@map ep
                             }
                         }
