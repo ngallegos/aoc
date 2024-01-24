dd75e946390daab0861e028a1e59e72ad6cc0f24
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-01-24 12:23:00 -0700

    Solved 2023 day 3 part 1 kotlin

--- 2023/kotlin/aoc-2023/src/test/kotlin/Day03Tests.kt
+++ 2023/kotlin/aoc-2023/src/test/kotlin/Day03Tests.kt
@@ -1,17 +1,36 @@
+import kotlin.test.assertEquals
+
 class Day03Tests : TestBase() {
     override fun SolvePart1_Sample() {
         // Arrange
+        val expectedSum = 4361;
         val engineParts = mutableListOf<EnginePart>();
-        get_sample().forEachIndexed { index, line -> engineParts.addAll(EnginePart.GetPartsFromSchematicLine(line, index)) }
+        val lines = get_sample().toList();
+        lines.forEachIndexed { index, line -> engineParts.addAll(EnginePart.getPartsFromSchematicLine(line, index, lines)) }
 
         // Act
-
+        val validEngineParts = engineParts.filter { it.isAdjacentToSymbol };
+        val actualSum = validEngineParts
+            .sumOf { it.partNumber };
 
         // Assert
+        assertEquals(expectedSum, actualSum);
     }
 
     override fun SolvePart1_Actual() {
-        TODO("Not yet implemented")
+        // Arrange
+        val expectedSum = 543867;
+        val engineParts = mutableListOf<EnginePart>();
+        val lines = get_input().toList();
+        lines.forEachIndexed { index, line -> engineParts.addAll(EnginePart.getPartsFromSchematicLine(line, index, lines)) }
+
+        // Act
+        val validEngineParts = engineParts.filter { it.isAdjacentToSymbol };
+        val actualSum = validEngineParts
+            .sumOf { it.partNumber };
+
+        // Assert
+        assertEquals(expectedSum, actualSum);
     }
 
     override fun SolvePart2_Sample() {
@@ -23,23 +42,52 @@ class Day03Tests : TestBase() {
     }
 
     class EnginePart{
-        public var PartNumber: Int = 0;
-        public var LineNumber: Int = 0;
-        public var PossibleSymbolIndices: MutableList<Int> = mutableListOf();
+        public var partNumber: Int = 0;
+        public var lineNumber: Int = 0;
+        public var isAdjacentToSymbol: Boolean = false;
         companion object {
-            fun GetPartsFromSchematicLine(line: String, lineNumber: Int): List<EnginePart>{
+            fun getPartsFromSchematicLine(line: String, lineNumber: Int, lines: List<String>): List<EnginePart>{
                 val partNumberRegex = Regex("\\d+");
+                val validSymbolRegex = Regex("[^\\d.]");
                 val engineParts = partNumberRegex.findAll(line)
                     .filter { it.value.isNotEmpty() }
                     .map {
                         val ep = EnginePart();
-                        ep.PartNumber = it.value.toInt();
-                        ep.LineNumber = lineNumber;
-                        if (it.range.first > 0)
-                            ep.PossibleSymbolIndices.add(it.range.first - 1);
-                        ep.PossibleSymbolIndices.addAll(it.range);
-                        if (it.range.last < line.length - 1)
-                            ep.PossibleSymbolIndices.add(it.range.last + 1);
+                        val possibleSymbolIndices = mutableListOf<Int>();
+                        ep.partNumber = it.value.toInt();
+                        ep.lineNumber = lineNumber;
+                        if (it.range.first > 0){
+                            if (line[it.range.first - 1] != '.') {
+                                ep.isAdjacentToSymbol = true;
+                                return@map ep;
+                            }
+                            possibleSymbolIndices.add(it.range.first - 1);
+                        }
+                        possibleSymbolIndices.addAll(it.range);
+                        if (it.range.last < line.length - 1) {
+                            if (line[it.range.last + 1] != '.'){
+                                ep.isAdjacentToSymbol = true;
+                                return@map  ep;
+                            }
+                            possibleSymbolIndices.add(it.range.last + 1);
+                        }
+
+                        if (lineNumber > 0){
+                            val prevLineToCheck = lines[lineNumber - 1].substring(possibleSymbolIndices.first(), possibleSymbolIndices.last() + 1);
+                            if (prevLineToCheck.any { c -> validSymbolRegex.matches(c.toString()) }){
+                                ep.isAdjacentToSymbol = true;
+                                return@map ep;
+                            }
+                        }
+
+                        if (lineNumber < lines.count() - 1){
+                            val nextLineToCheck = lines[lineNumber + 1].substring(possibleSymbolIndices.first(), possibleSymbolIndices.last() + 1);
+                            if (nextLineToCheck.any { c -> validSymbolRegex.matches(c.toString()) }){
+                                ep.isAdjacentToSymbol = true;
+                                return@map ep;
+                            }
+                        }
+
                         return@map ep;
                     }.toList();
                 return engineParts;
