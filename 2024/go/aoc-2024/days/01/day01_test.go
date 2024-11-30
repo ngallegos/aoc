8892b8af87704c425bfe2a22cde4397b1db476ca
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-11-30 10:10:56 -0700

    2024 got inputs reading

--- 2024/go/aoc-2024/days/01/day01_test.go
+++ 2024/go/aoc-2024/days/01/day01_test.go
@@ -1,23 +1,49 @@
-package main
+package day01
 
 import (
 	"bufio"
-	"fmt"
+	"log"
 	"os"
 	"testing"
 )
 
-func Part1() string {
-	fmt.Println("day 1")
-	return "day 1"
+func Part1(isSample bool) string {
+	var lines []string
+	var err error
+	if isSample {
+		lines, err = readSample()
+	} else {
+		lines, err = readInput()
+	}
+
+	log.Println(err)
+	if &err != nil {
+		return lines[0]
+	}
+	return "failed to read"
 }
 
 func TestPart1(t *testing.T) {
-	result := Part1()
-	if result != "day 1" {
+	result := Part1(false)
+	if result != "day 01" {
 		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "day 1")
 	}
 
+}
+
+func TestSamplePart1(t *testing.T) {
+	result := Part1(true)
+	if result != "line1" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "line1")
+	}
+}
+
+func readInput() ([]string, error) {
+	return readLines(`./input.txt`)
+}
+
+func readSample() ([]string, error) {
+	return readLines(`./sample.txt`)
 }
 
 func readLines(path string) ([]string, error) {
@@ -32,5 +58,6 @@ func readLines(path string) ([]string, error) {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
+	log.Println(lines)
 	return lines, scanner.Err()
 }
