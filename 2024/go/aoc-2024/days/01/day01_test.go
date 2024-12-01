209e5773dfff61daf6650ee29893384b12dc918f
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-01 08:46:35 -0700

    2024 day 01 part 2

--- 2024/go/aoc-2024/days/01/day01_test.go
+++ 2024/go/aoc-2024/days/01/day01_test.go
@@ -34,6 +34,13 @@ func Part1(isSample bool) int {
 	return -1
 }
 
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
 func TestPart1(t *testing.T) {
 	result := Part1(false)
 	if result != 2367773 {
@@ -49,11 +56,55 @@ func TestSamplePart1(t *testing.T) {
 	}
 }
 
-func absDiffInt(x, y int) int {
-	if x < y {
-		return y - x
+func Part2(isSample bool) int64 {
+	var col1 []int
+	var col2 []int
+	var err error
+	if isSample {
+		col1, col2, err = readSample()
+	} else {
+		col1, col2, err = readInput()
 	}
-	return x - y
+
+	//log.Println(err)
+	if &err != nil {
+		slices.Sort(col1)
+		slices.Sort(col2)
+		sum := int64(0)
+
+		for i := 0; i < len(col1); i++ {
+			sum += int64(col1[i]) * numOccurences(col1[i], col2)
+		}
+
+		return sum
+	}
+	return -1
+}
+
+func numOccurences(target int, pool []int) int64 {
+	occurences := int64(0)
+	for _, i := range pool {
+		if i == target {
+			occurences++
+		}
+	}
+
+	return occurences
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(false)
+	if result != 21271939 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "21271939")
+	}
+
+}
+
+func TestSamplePart2(t *testing.T) {
+	result := Part2(true)
+	if result != 31 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "31")
+	}
 }
 
 func readInput() ([]int, []int, error) {
