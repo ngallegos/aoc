bcf23b61a23f0cd8d4e0306e8b451d422bd3822f
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-02 08:06:38 -0700

    2024 day 02 part 1

--- 2024/go/aoc-2024/days/02/day02_test.go
+++ 2024/go/aoc-2024/days/02/day02_test.go
@@ -16,21 +16,57 @@ func Part1(isSample bool) int {
 	} else {
 		reports, err = readInput()
 	}
-	return 0
+
+	safeReports := 0
+
+	if &err != nil {
+		for i := 0; i < len(reports); i++ {
+			report := reports[i]
+			direction := 0
+			reportIsSafe := true
+			for j := 0; j < (len(report) - 1); j++ {
+				prevDirection := direction
+				diff, curDirection := absDiffInt(report[j], report[j+1])
+				direction = curDirection
+				//log.Printf("%d -> %d", prevDirection, direction)
+				if diff < 1 || diff > 3 || (prevDirection != direction && prevDirection != 0) {
+					reportIsSafe = false
+					break
+				}
+			}
+			if reportIsSafe {
+				//log.Println(report)
+				safeReports++
+			}
+		}
+
+		return safeReports
+	}
+
+	panic(err)
+}
+
+func absDiffInt(x, y int) (int, int) {
+	if x < y {
+		return y - x, 1
+	} else if y < x {
+		return x - y, -1
+	}
+	return x - y, -2
 }
 
 func TestPart1(t *testing.T) {
 	result := Part1(false)
-	if result != 0 {
-		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "0")
+	if result != 369 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "369")
 	}
 
 }
 
 func TestSamplePart1(t *testing.T) {
 	result := Part1(true)
-	if result != 0 {
-		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "0")
+	if result != 2 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "2")
 	}
 }
 
@@ -70,4 +106,6 @@ func toIntArray(input []string) []int {
 		}
 		result = append(result, intVal)
 	}
+
+	return result
 }
