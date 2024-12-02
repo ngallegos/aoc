a17146a44df4a87607bebd3a4f99a756f7198f42
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-02 16:48:14 -0700

    2024 day 02 part 2 sample working...

--- 2024/go/aoc-2024/days/02/day02_test.go
+++ 2024/go/aoc-2024/days/02/day02_test.go
@@ -2,7 +2,9 @@ package day02
 
 import (
 	"bufio"
+	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"testing"
@@ -67,6 +69,96 @@ func TestSamplePart1(t *testing.T) {
 	result := Part1(true)
 	if result != 2 {
 		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "2")
+	}
+}
+
+func Part2(isSample bool) int {
+	var reports [][]int
+	var err error
+	if isSample {
+		reports, err = readSample()
+	} else {
+		reports, err = readInput()
+	}
+
+	safeReports := 0
+
+	if &err != nil {
+		for i := 0; i < len(reports); i++ {
+			report := reports[i]
+			direction := 0
+			reportIsSafe := true
+			unsafeReportFirstIndex := -1
+			for j := 0; j < (len(report) - 1); j++ {
+				prevDirection := direction
+				diff, curDirection := absDiffInt(report[j], report[j+1])
+				direction = curDirection
+				//log.Printf("%d -> %d", prevDirection, direction)
+				if diff < 1 || diff > 3 || (prevDirection != direction && prevDirection != 0) {
+					unsafeReportFirstIndex = j
+					break
+				}
+			}
+
+			if unsafeReportFirstIndex > -1 {
+				moddedReport := slices.Clone(report)
+				moddedReport = append(moddedReport[:unsafeReportFirstIndex], moddedReport[unsafeReportFirstIndex+1:]...)
+				log.Println(report)
+				for j := 0; j < (len(moddedReport) - 1); j++ {
+					prevDirection := direction
+					diff, curDirection := absDiffInt(moddedReport[j], moddedReport[j+1])
+					direction = curDirection
+					//log.Printf("%d -> %d", prevDirection, direction)
+					if diff < 1 || diff > 3 || (prevDirection != direction && prevDirection != 0) {
+						log.Printf("first chance: %v", moddedReport)
+						reportIsSafe = false
+						break
+					}
+				}
+			}
+
+			if !reportIsSafe && unsafeReportFirstIndex < len(report)-1 {
+				reportIsSafe = true
+				moddedReport := slices.Clone(report)
+				moddedReport = append(moddedReport[:unsafeReportFirstIndex+1], moddedReport[unsafeReportFirstIndex+2:]...)
+				log.Println(report)
+				for j := 0; j < (len(moddedReport) - 1); j++ {
+					prevDirection := direction
+					diff, curDirection := absDiffInt(moddedReport[j], moddedReport[j+1])
+					direction = curDirection
+					//log.Printf("%d -> %d", prevDirection, direction)
+					if diff < 1 || diff > 3 || (prevDirection != direction && prevDirection != 0) {
+						log.Printf("last chance: %v", moddedReport)
+						reportIsSafe = false
+						break
+					}
+				}
+			}
+
+			if reportIsSafe {
+				//log.Println(report)
+				safeReports++
+			}
+		}
+
+		return safeReports
+	}
+
+	panic(err)
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(false)
+	if result != 369 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "369")
+	}
+
+}
+
+func TestSamplePart2(t *testing.T) {
+	result := Part2(true)
+	if result != 4 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "4")
 	}
 }
 
