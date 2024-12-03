95d5ae631a9f97709b522195592bff559dea8155
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-03 16:29:43 -0700

    2024 day 03 part 2 attempt

--- 2024/go/aoc-2024/days/03/day03_test.go
+++ 2024/go/aoc-2024/days/03/day03_test.go
@@ -56,6 +56,62 @@ func TestSamplePart1(t *testing.T) {
 	}
 }
 
+func Part2(isSample bool) int64 {
+	var corruptedMemory []string
+	var err error
+	if isSample {
+		corruptedMemory, err = readSample()
+	} else {
+		corruptedMemory, err = readInput()
+	}
+
+	results := int64(0)
+
+	if &err != nil {
+		r := regexp.MustCompile(`mul\((\d+),(\d+)\)|do(n't)?\(\)`)
+		for i := 0; i < len(corruptedMemory); i++ {
+			matches := r.FindAllStringSubmatch(corruptedMemory[i], -1)
+			//log.Println(matches)
+			enabled := true
+			for _, m := range matches {
+				if m[0] == "do()" {
+					enabled = true
+					continue
+				} else if m[0] == "don't()" {
+					enabled = false
+					continue
+				} else if enabled {
+					arg1, err1 := strconv.Atoi(m[1])
+					arg2, err2 := strconv.Atoi(m[2])
+					if err1 == nil && err2 == nil {
+						results += int64(arg1) * int64(arg2)
+					}
+				}
+			}
+
+		}
+
+		return results
+	}
+
+	panic(err)
+}
+
+func TestPart2(t *testing.T) {
+	result := Part2(false)
+	if result != 175015740 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "175015740")
+	}
+
+}
+
+func TestSamplePart2(t *testing.T) {
+	result := Part2(true)
+	if result != 48 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "48")
+	}
+}
+
 func readInput() ([]string, error) {
 	return readLines(`./input.txt`)
 }
