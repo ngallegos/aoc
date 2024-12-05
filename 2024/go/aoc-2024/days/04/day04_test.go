b23c267251220b0809ecc89877b4c0e583fabbe1
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-04 19:32:27 -0700

    2024 day 04 I'm terrible at these kinds of puzzles -_-

--- 2024/go/aoc-2024/days/04/day04_test.go
+++ 2024/go/aoc-2024/days/04/day04_test.go
@@ -2,6 +2,7 @@ package day04
 
 import (
 	"bufio"
+	"log"
 	"os"
 	"testing"
 )
@@ -15,15 +16,125 @@ func Part1(isSample bool) int {
 		lines, err = readInput()
 	}
 
-	results := -1
+	results := 0
 
 	if &err != nil && len(lines) > 0 {
-		// todo - implement
+		for y := 0; y < len(lines); y++ {
+			line := lines[y]
+			for x := 0; x < len(line); x++ {
+				if line[x] == 'X' {
+					results += findXMASInRadius(x, y, lines)
+				}
+			}
+		}
 
 		return results
 	}
 
 	panic(err)
+}
+
+func findXMASInRadius(x int, y int, wordsearch []string) int {
+	keyword := "XMAS"
+	kLength := len(keyword)
+	xmin := 0
+	xmax := len(wordsearch[0]) - 1
+	ymin := 0
+	ymax := len(wordsearch) - 1
+
+	instances := 0
+
+	log.Printf("(%d, %d)", x, y)
+	if x-kLength+1 >= xmin {
+		//log.Println(Reverse(wordsearch[y][x-kLength+1 : x+1]))
+		if Reverse(wordsearch[y][x-kLength+1:x+1]) == keyword {
+			instances++
+		}
+
+		if y-kLength+1 >= ymin {
+			word := ""
+			for i := 0; i < kLength; i++ {
+				word += string(wordsearch[y-i][x-i])
+			}
+			if Reverse(word) == keyword {
+				instances++
+			}
+		}
+
+		if y+kLength-1 <= ymax {
+			word := ""
+			for i := 0; i < kLength; i++ {
+				word = string(wordsearch[y+i][x-i])
+			}
+			if Reverse(word) == keyword {
+				instances++
+			}
+
+		}
+	}
+
+	if x+kLength-1 <= xmax {
+
+		if wordsearch[y][x:x+kLength] == keyword {
+			//log.Println(wordsearch[y][x : x+kLength])
+			instances++
+		}
+
+		if y-kLength+1 >= ymin {
+			word := ""
+			for i := 0; i < kLength; i++ {
+				word += string(wordsearch[y-i][x+i])
+			}
+			//log.Println(word)
+			if word == keyword {
+				instances++
+			}
+		}
+
+		if y+kLength-1 <= ymax {
+			runes := ""
+			for i := 0; i < kLength; i++ {
+				runes += string(wordsearch[y+i][x+i])
+			}
+			//log.Println(runes)
+			if runes == keyword {
+				instances++
+			}
+
+		}
+	}
+
+	if y-kLength+1 >= ymin {
+		word := ""
+		for i := 0; i < kLength; i++ {
+			word += string(wordsearch[y-i][x])
+		}
+		//log.Println(word)
+		if Reverse(word) == keyword {
+			instances++
+		}
+	}
+
+	if y+kLength-1 <= ymax {
+		word := ""
+		for i := 0; i < kLength; i++ {
+			word += string(wordsearch[y+i][x])
+		}
+		//log.Println(word)
+		if word == keyword {
+			instances++
+		}
+	}
+
+	return instances
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
 
 func TestPart1(t *testing.T) {
@@ -36,8 +147,8 @@ func TestPart1(t *testing.T) {
 
 func TestSamplePart1(t *testing.T) {
 	result := Part1(true)
-	if result != 0 {
-		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "0")
+	if result != 18 {
+		t.Errorf("Result was incorrect, got: %d, want: %s.", result, "18")
 	}
 }
 
