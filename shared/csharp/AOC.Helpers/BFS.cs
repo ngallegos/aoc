b415cceb580ef1d0670b35cbf1c81fdda053e057
Nick Gallegos <nicholas.gallegos@gmail.com>
2024-12-28 15:12:02 -0700

    Trying to get some shared code in place for use across the years

---
+++ shared/csharp/AOC.Helpers/BFS.cs
@@ -0,0 +1,80 @@
+﻿// ReSharper disable InconsistentNaming
+namespace AOC.Helpers;
+
+public class BFS<T> where T : IEquatable<T>
+{
+    public static List<T>? Search(T start, Func<T, List<T>> getAdjacent, 
+        Func<T, bool> destinationReached)
+    {
+        var queue = new Queue<BFSNode<T>>();
+        var checkedNodes = new HashSet<BFSNode<T>>();
+        var startNode = new BFSNode<T>(start);
+        checkedNodes.Add(startNode);
+        queue.Enqueue(startNode);
+        BFSNode<T> node;
+        while (queue.Count > 0)
+        {
+            node = queue.Dequeue();
+            if (destinationReached(node.Item))
+                return node.GetPath();
+            var adjacentItems = getAdjacent(node.Item);
+
+            foreach (var adjacent in adjacentItems)
+            {
+                var adjacentNode = new BFS<T>.BFSNode<T>(adjacent);
+                if (checkedNodes.Add(adjacentNode))
+                {
+                    adjacentNode.Parent = node;
+                    queue.Enqueue(adjacentNode);
+                }
+            }
+        };
+
+        return null;
+    }
+
+    private class BFSNode<T> : IEquatable<BFSNode<T>> where T : IEquatable<T>
+    {
+        public T Item { get; }
+        public BFSNode<T> Parent { get; set; }
+
+        public BFSNode(T item)
+        {
+            Item = item;
+        }
+
+        public List<T> GetPath()
+        {
+            var path = new List<T>();
+            var current = this;
+            while (current != null)
+            {
+                path.Add(current.Item);
+                current = current.Parent;
+            }
+
+            path.Reverse();
+            return path;
+        }
+
+        public bool Equals(BFSNode<T> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Item.Equals(other.Item);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((BFSNode<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Item.GetHashCode();
+        }
+    }
+}
---
+++ shared/csharp/AOC.Helpers/Coordinate.cs
@@ -0,0 +1,24 @@
+﻿namespace AOC.Helpers;
+
+public class Coordinate(int x, int y)
+{
+    public int X { get; protected set; } = x;
+    public int Y { get; protected set; } = y;
+
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+
+    public static Coordinate operator +(Coordinate a, Coordinate b) => new(a.X + b.X, a.Y + b.Y);
+    public static Coordinate operator +(Coordinate a, (int x, int y) b) => new(a.X + b.x, a.Y + b.y);
+}
+
+public class Coordinate<T> : Coordinate
+{
+    public Coordinate(int x, int y, T value) : base(x, y)
+    {
+        Value = value;
+    }
+    public T Value { get; protected set; }
+}
