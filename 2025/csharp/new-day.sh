2053fe9429bc095eb970be3e9599fda6cd750d62
Nick Gallegos <nick.gallegos@particular.net>
2025-12-08 10:00:14 -0700

    Putting in some more scaffolding

--- 2025/csharp/new-day.sh
+++ 2025/csharp/new-day.sh
@@ -107,21 +107,45 @@ public class Day${day_padded}Tests : TestBase
 {
     protected override void SolvePart1_Sample()
     {
+        // Arrange
+        var _ = get_sample().ToList();
+        
+        // Act
+        
+        // Assert
         throw new System.NotImplementedException();
     }
 
     protected override void SolvePart1_Actual()
     {
+        // Arrange
+        var _ = get_input().ToList();
+        
+        // Act
+        
+        // Assert
         throw new System.NotImplementedException();
     }
 
     protected override void SolvePart2_Sample()
     {
+        // Arrange
+        var _ = get_sample().ToList();
+        
+        // Act
+        
+        // Assert
         throw new System.NotImplementedException();
     }
 
     protected override void SolvePart2_Actual()
     {
+        // Arrange
+        var _ = get_input().ToList();
+        
+        // Act
+        
+        // Assert
         throw new System.NotImplementedException();
     }
 }
