9f627adf2a16d18cad42f98f1543567b7a5b5d99
Nick Gallegos <nick.gallegos@particular.net>
2025-12-07 19:48:29 -0700

    2025-12-07 setup

---
+++ 2025/csharp/AOC2025.Tests/Day07Tests.cs
@@ -0,0 +1,24 @@
+namespace AOC2025.Tests;
+
+public class Day07Tests : TestBase
+{
+    protected override void SolvePart1_Sample()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void SolvePart1_Actual()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void SolvePart2_Sample()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void SolvePart2_Actual()
+    {
+        throw new System.NotImplementedException();
+    }
+}
