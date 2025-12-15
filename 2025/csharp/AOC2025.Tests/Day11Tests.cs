1015bbb905806c05c3e67b39081a2c48907c6eb6
Nick Gallegos <nick.gallegos@particular.net>
2025-12-14 20:21:34 -0700

    2025-12-11 part 1 solved

--- 2025/csharp/AOC2025.Tests/Day11Tests.cs
+++ 2025/csharp/AOC2025.Tests/Day11Tests.cs
@@ -2,28 +2,36 @@ namespace AOC2025.Tests;
 
 public class Day11Tests : TestBase
 {
-    [Ignore("Not attempted yet")]
     protected override void SolvePart1_Sample()
     {
         // Arrange
-        var _ = get_sample().ToList();
+        var devices = get_sample(x =>
+        {
+            var device = x.Split(":", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return new Device(device[0], device[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        }).ToList();
         
         // Act
+        var pathCount = FindPaths("you", "out", devices.ToArray());
         
         // Assert
-        throw new System.NotImplementedException();
+        pathCount.ShouldBe(5);
     }
 
-    [Ignore("Not attempted yet")]
     protected override void SolvePart1_Actual()
     {
         // Arrange
-        var _ = get_input().ToList();
+        var devices = get_input(x =>
+        {
+            var device = x.Split(":", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return new Device(device[0], device[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        }).ToList();
         
         // Act
+        var pathCount = FindPaths("you", "out", devices.ToArray());
         
         // Assert
-        throw new System.NotImplementedException();
+        pathCount.ShouldBe(696L);
     }
 
     [Ignore("Not attempted yet")]
@@ -49,4 +57,20 @@ public class Day11Tests : TestBase
         // Assert
         throw new System.NotImplementedException();
     }
+
+    long FindPaths(string start, string end, Device[] devices, long count = 0)
+    {
+        if (start == end)
+            return 1;
+        
+        var startDevice = devices.First(x => x.Id == start);
+        foreach (var output in startDevice.Outputs)
+        {
+            count += FindPaths(output, end, devices);
+        }
+
+        return count;
+    }
+    
+    record Device(string Id, string[] Outputs);
 }
