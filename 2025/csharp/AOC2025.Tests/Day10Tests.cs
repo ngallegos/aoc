ac47f0e9639ab25504d8e27203c89c2651161beb
Nick Gallegos <nicholas.gallegos@gmail.com>
2025-12-20 08:20:54 -0500

    2025-12-10 input parsing

--- 2025/csharp/AOC2025.Tests/Day10Tests.cs
+++ 2025/csharp/AOC2025.Tests/Day10Tests.cs
@@ -2,11 +2,10 @@ namespace AOC2025.Tests;
 
 public class Day10Tests : TestBase
 {
-    [Ignore("Not attempted yet")]
     protected override void SolvePart1_Sample()
     {
         // Arrange
-        var _ = get_sample().ToList();
+        var machineDefinitions = get_sample(x => new Machine(x)).ToList();
         
         // Act
         
@@ -48,5 +47,20 @@ public class Day10Tests : TestBase
         
         // Assert
         throw new System.NotImplementedException();
+    }
+
+    class Machine
+    {
+        public Machine(string definition)
+        {
+            var parts = definition.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            LightDiagram = parts[0].Trim('[', ']');
+            WiringSchematics = parts[1..^1].Select(x => x.Trim('(', ')').Split(',').Select(int.Parse).ToArray()).ToArray();
+            JoltageRequirements = parts[^1].Trim('{', '}').Split(',').Select(int.Parse).ToArray();
+        }
+
+        string LightDiagram { get; }
+        int[][] WiringSchematics { get; }
+        int[] JoltageRequirements { get; }
     }
 }
