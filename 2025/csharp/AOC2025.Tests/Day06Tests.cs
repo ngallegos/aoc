6fad941dfa4b4fb04ca1dba2ca251688433dd79a
Nick Gallegos <nicholas.gallegos@gmail.com>
2025-12-23 16:27:52 -0500

    2025-12-06 solved part 2

--- 2025/csharp/AOC2025.Tests/Day06Tests.cs
+++ 2025/csharp/AOC2025.Tests/Day06Tests.cs
@@ -30,16 +30,32 @@ public class Day06Tests : TestBase
         grandTotal.ShouldBe(5381996914800L);
     }
 
-    [Ignore("Coming back to this...")]
     protected override void SolvePart2_Sample()
     {
-        throw new System.NotImplementedException();
+        // Arrange
+        var worksheet = get_sample().ToList();
+        
+        // Act
+        var runningTotals = GetRunningTotals_ColumnRightToLeft(worksheet);
+        
+        var grandTotal = runningTotals.Sum();
+        
+        // Assert
+        grandTotal.ShouldBe(3263827L);
     }
 
-    [Ignore("Coming back to this...")]
     protected override void SolvePart2_Actual()
     {
-        throw new System.NotImplementedException();
+        // Arrange
+        var worksheet = get_input().ToList();
+        
+        // Act
+        var runningTotals = GetRunningTotals_ColumnRightToLeft(worksheet);
+        
+        var grandTotal = runningTotals.Sum();
+        
+        // Assert
+        grandTotal.ShouldBe(9627174150897L);
     }
 
     long[] GetRunningTotals_RowLeftToRight(List<string> worksheet)
@@ -66,7 +82,73 @@ public class Day06Tests : TestBase
                         runningTotals[j] *= numbers[i][j];
                         break;
                     default:
-                        throw new System.Exception($"Unknown operator {operators[j]}");
+                        throw new Exception($"Unknown operator {operators[j]}");
+                }
+            }
+        }
+        
+        return runningTotals;
+    }
+    
+    long[] GetRunningTotals_ColumnRightToLeft(List<string> worksheet)
+    {
+        var operatorLine = worksheet.Last();
+        var operators = operatorLine.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+        var columnWidths = new int[operators.Length];
+
+        var col = 0;
+        foreach (var c in operatorLine.Skip(1))
+        {
+            if (c != ' ')
+                col++;
+            else
+                columnWidths[col]++;
+        }
+
+        columnWidths[^1]++;
+
+        var currentIndex = 0;
+        var equations = new int[columnWidths.Length][];
+        foreach (var cw in columnWidths.Select((x, i) => new { width = x, ix = i }))
+        {
+            var numbers = new int[cw.width];
+
+            for (var j = cw.width-1; j >= 0; j--)
+            {
+                var numString = "";
+                for (var i = 0; i < worksheet.Count - 1; i++)
+                {
+                    numString += worksheet[i][j + currentIndex];
+                }
+                
+                numbers[j] = int.Parse(numString.Replace(" ", ""));
+            }
+            
+            equations[cw.ix] = numbers;
+            currentIndex += cw.width + 1;
+        }
+        
+        var runningTotals = new long[columnWidths.Length];
+
+        foreach (var eq in equations.Select((x, i) => new { equation = x, ix = i }))
+        {
+            var op = operators[eq.ix];
+            foreach (var n in eq.equation.Select((x, i) => new { num = x, ix = i }))
+            {
+                switch (op)
+                {
+                    case "+":
+                        if (n.ix == 0)
+                            runningTotals[eq.ix] = 0;
+                        runningTotals[eq.ix] += n.num;
+                        break;
+                    case "*":
+                        if (n.ix == 0)
+                            runningTotals[eq.ix] = 1;
+                        runningTotals[eq.ix] *= n.num;
+                        break;
+                    default:
+                        throw new Exception($"Unknown operator {op}");
                 }
             }
         }
